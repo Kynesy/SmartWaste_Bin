65f9f9c14619f3d568a0c4109f029a0c1eb2747e
Cesare <100310104+Kynesy@users.noreply.github.com>
2023-08-25 19:45:52 +0200

    test mock del servizio

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import services.IPublisherService;
+import services.PublisherService;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        IPublisherService publisherService = new PublisherService();
+        publisherService.sendTrash("{ 'id': 'pippo'}");
     }
 }
